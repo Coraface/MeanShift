fda45870ae8ea19c5b5e20cd9e2954c8a9c62b0e
Coraface <35435049+Coraface@users.noreply.github.com>
2018-02-12 14:28:38 +0200

    Parallel Mean Shift

--- MeanShift.cu
+++ MeanShift.cu
@@ -29,7 +29,7 @@ __global__ void kernel(int *, int *, double *, double *, double *);
 /****-------------Main programm-------------****/
 int main(int argc, char** argv)
 {
-	
+ 	
   if(argc != 2){
     printf("Usage : No file name given...\n");
     exit(0);
